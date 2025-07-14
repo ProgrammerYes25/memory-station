b439bb4da4eac92a8883c06d2f9e6b1e30f95189
ProgrammerYes25 <pogrammer.yes25@gmail,com>
2025-07-14 23:01:26 +0900

    파일 기본 세팅 와료

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,25 @@
-import { useEffect, useState } from 'react';
-import './styles/App.css';
+import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Test from './pages/Test';
+import Test from './tests/Test';
+import Home from './pages/HomePage'
+import Goal from './pages/GoalPage'
+import Login from './pages/LoginPage'
+import Review from './pages/ReviewPage'
+import Sigup from './pages/SignupPage'
 
 function App() {
-  // const [message, setMessage] = useState("");
 
-  // useEffect(()=>{
-  //   fetch("/backend/test")
-  //     .then(res => res.text())
-  //     .then( m=> setMessage(m))
-  // }, [])
 
   console.log("테스트")
   return (
     <div className="App">
       <BrowserRouter basename='/frontend'>
         <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/goal" element={<Goal />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/review" element={<Review />}/>
+          <Route path="/sigup" element={<Sigup />}/>
           <Route path="/test" element={<Test />}/>
         </Routes>
       </BrowserRouter>
